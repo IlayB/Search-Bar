5ae0aa088206f4c078151e0c77e94955dfa9d5ba
IlayB <ilayb776@gmail.com>
2021-06-01 18:46:41 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import "./App.css";
 
 function App() {
   const [search, setSearch] = useState("");
-  const [result, setResult] = useState("");
-
+  const [result, setResult] = useState([]);
+  const [filtered, setFiltered] = useState([]);
   useEffect(() => {
     fetch("https://run.mocky.io/v3/fa120fc2-a6cb-4481-baf0-a66e8146b367")
       .then((res) => res.json())
-      .then((res) => {
-        setResult(
-          Object.values(res.DATASET).filter((data) =>
-            data.toLowerCase().includes(search.trim().toLowerCase())
-          )
-        );
-      });
-  }, [search]);
+      .then((res) => setResult(res.DATASET));
+  }, []);
+  useEffect(() => {
+    const filteredResults = result.filter((name) =>
+      name.toLowerCase().includes(search.trim().toLowerCase())
+    );
+    return setFiltered(filteredResults);
+  }, [search, result]);
+
   return (
     <div className="App">
       <div>Search</div>
       <input type="text" onChange={(text) => setSearch(text.target.value)} />
-      {Object.values(result).map(function (name) {
-        return <div style={{ color: "red" }}>{name}</div>;
-      })}
+      {filtered.map((name, index) => (
+        <div key={index} style={{ color: "red" }}>
+          {name}
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
