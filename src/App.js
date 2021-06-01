fcc310596e07cb1bf3a00ad1919ff83cff99abf2
IlayB <ilayb776@gmail.com>
2021-06-01 17:35:28 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    fetch("https://run.mocky.io/v3/fa120fc2-a6cb-4481-baf0-a66e8146b367")
+      .then((res) => res.json())
+      .then((res) => {
+        setResult(
+          Object.values(res.DATASET).filter((data) =>
+            data.toLowerCase().includes(search.trim().toLowerCase())
+          )
+        );
+      });
+  }, [search]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Search</div>
+      <input type="text" onChange={(text) => setSearch(text.target.value)} />
+      {Object.values(result).map(function (name) {
+        return <div>{name}</div>;
+      })}
     </div>
   );
 }
